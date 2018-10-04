7abfa4bc68eb988ffca404c896a701cef98401ee
adamteaney <adamteaney@gmail.com>
2018-10-03 19:37:50 -0500

    Created class AutonomousStart, which is a framework for the start of the autonomous section of the robot game.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousStart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousStart.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Range;
+
 public class AutonomousStart extends LinearOpMode {
+
+    //TODO: Refine headingDrive(), find actual values instead of placeholders, eventually
 
     MyHardwarePushbot robot = new MyHardwarePushbot();
 
@@ -19,24 +23,33 @@ public class AutonomousStart extends LinearOpMode {
 
         // Lower the lift that holds the robot to the lander.
 
-        int LiftTarget = robot.leftDrive.getCurrentPosition() + PlACEHOLDER;
-
-        robot.leftArm.setTargetPosition(LiftTarget);
-
-        robot.leftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        robot.leftArm.setPower(PLACEHOLDER);
-
-        while (robot.leftArm.isBusy()) {
-
-        }
-
-        robot.leftArm.setPower(0);
-        robot.leftArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Raise the hook off of the lander handle. Not sure about this one yet.
+        moveLift(PLACEHOLDER, PLACEHOLDER);
 
         // Drive away *slightly* from the lander.
+
+        drive(PLACEHOLDER, PLACEHOLDER);
+
+        // Lower the lift back to the robot.
+
+        moveLift(PLACEHOLDER, PLACEHOLDER);
+
+        // Drive out of the lander zone, but not so far as to disturb the minerals on the field.
+
+        drive(PLACEHOLDER, PLACEHOLDER);
+
+        // Turn 90 degrees.
+
+        headingDrive(PLACEHOLDER, PLACEHOLDER, PLACEHOLDER);
+
+        // Drive forward to bypass the minerals on the field.
+
+        drive(PLACEHOLDER, PLACEHOLDER);
+
+        //Turn 225 degrees.
+
+        headingDrive(PLACEHOLDER, PLACEHOLDER, PLACEHOLDER);
+
+        // Drive forward into the base.
 
         drive(PLACEHOLDER, PLACEHOLDER);
 
@@ -96,6 +109,9 @@ public class AutonomousStart extends LinearOpMode {
 
     public void moveLift(double speed, double distance) {
 
+        // This is pretty much a carbon copy of the above drive() function, but used only for the lift on the robot.
+
+        // Define the target for the lift to reach.
         int moveCounts = (int) (distance * 1);
         int LiftTarget = robot.leftDrive.getCurrentPosition() + moveCounts;
 
@@ -111,25 +127,59 @@ public class AutonomousStart extends LinearOpMode {
         }
 
         robot.leftArm.setPower(0);
+
         robot.leftArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-
 
     }
 
+    public void headingDrive(double speed, double leftDistance, double rightDistance) {
+
+        if (opModeIsActive()) {
+
+            // Define the target for the robot to reach.
+            int leftMoveCounts = (int) (distance * 1);
+            int rightMoveCounts = (int) (distance * 1);
+            int newLeftTarget = robot.leftDrive.getCurrentPosition() + moveCounts;
+            int newRightTarget = robot.rightDrive.getCurrentPosition() + moveCounts;
+
+            // Set the target positions for the robot, and set the robots to target mode.
+            robot.leftDrive.setTargetPosition(newLeftTarget);
+            robot.leftFrontDrive.setTargetPosition(newLeftTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+            robot.rightFrontDrive.setTargetPosition(newRightTarget);
+
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set the speed of the robot to the "speed" variable in the function.
+            speed = Range.clip(Math.abs(speed), 0.0, 1.0);
+            robot.leftDrive.setPower(speed);
+            robot.leftFrontDrive.setPower(speed);
+            robot.rightDrive.setPower(speed);
+            robot.rightFrontDrive.setPower(speed);
 
 
-    // Lower the lift back to the robot.
+            while (opModeIsActive() &&
+                    robot.leftDrive.isBusy() && robot.leftFrontDrive.isBusy() && robot.rightDrive.isBusy() && robot.rightFrontDrive.isBusy()) {
+            }
 
-    // Drive out of the lander zone, but not so far as to disturb the minerals on the field.
+            // Set the robot's power to 0.
+            robot.leftDrive.setPower(0);
+            robot.leftFrontDrive.setPower(0);
+            robot.rightDrive.setPower(0);
+            robot.rightFrontDrive.setPower(0);
 
-    // Turn 90 degrees.
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-    // Drive forward to bypass the minerals on the field.
+        }
 
-    // Turn 225 degrees.
 
-    // Drive forward into the base.
+    }
 
 }
 
