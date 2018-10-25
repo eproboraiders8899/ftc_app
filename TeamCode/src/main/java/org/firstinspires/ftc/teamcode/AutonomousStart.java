88210da3d9c9e316c02d7d5573e891839802a670
adamteaney <adamteaney@gmail.com>
2018-10-24 19:38:33 -0500

    - Created two subclasses off of AutonomousStart called BaseStart and Crater Start. AutonomousStart has all of the "universal" code such as functions.
    - Refined AutonomousStart so that the functions inside use actual inches as measurements! However, all of the movements will be drastically off for the time being.
    
    - Added new motor linearLift. This motor serves no purpose, yet...
    
    - Cleaned up code slightly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousStart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousStart.java
@@ -11,81 +11,26 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import static java.lang.Thread.sleep;
 
-@Autonomous(name = "AutonomousStart", group = "Autonomous")
 // @Disabled
 public class AutonomousStart extends LinearOpMode {
+
+    public void runOpMode() {}
 
     private ElapsedTime runtime = new ElapsedTime();
 
     double liftPower = 0;
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 4);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
+    // static final double COUNTS_PER_MOTOR_REV  = 1440;
+    // static final double DRIVE_GEAR_REDUCTION  = 2.0;
+    // static final double WHEEL_DIAMETER_INCHES = 4.0;
+    // static final double COUNTS_PER_INCH       = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES);
+    static final double COUNTS_PER_MOTOR_REV     = 1120;
+    static final double WHEEL_RADIUS_INCHES      = 2.0;
+    static final double COUNTS_PER_INCH          = (COUNTS_PER_MOTOR_REV) / (WHEEL_RADIUS_INCHES * Math.PI * 2);
+    static final double DRIVE_SPEED              = 0.6;
+    static final double TURN_SPEED               = 0.5;
 
     MyHardwarePushbot robot = new MyHardwarePushbot();
-
-    public void runOpMode() {
-
-        robot.init(hardwareMap);
-
-        // Create a placeholder variable to be used in place of actual distances. THIS IS TEMPORARY!
-        int placeholder = 1;
-
-        telemetry.addData(">", "Robot Ready.");
-        telemetry.update();
-
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
-        // Lower the lift that holds the robot to the lander.
-
-        moveLift(1, 3.4);
-
-        // Drive away *slightly* from the lander.
-
-        turn(-.5, .5);
-
-        drive(.5, -180);
-
-        turn(-.5, 1.5);
-
-
-
-        // Drive out of the lander zone, but not so far as to disturb the minerals on the field.
-
-        drive(1, 360);
-         encoderDrive(DRIVE_SPEED,  -8.5,  -8.5, 2.0);  // S1: Forward 12 Inches with 5 Sec timeout
-
-
-        // Turn 90 degrees.
-        encoderDrive(DRIVE_SPEED,  7,  -7, 2.0);  // S1: turn 90 left with 5 Sec timeout
-
-
-        // Drive forward to bypass the minerals on the field.
-        encoderDrive(DRIVE_SPEED,  -15,  -15  , 2.0);  // S1: Forward 12 Inches with 5 Sec timeout
-
-        // Turn 225 degrees.
-        encoderDrive(DRIVE_SPEED,  -7,  7, 2.0);  // S1: Forward 12 Inches with 5 Sec timeout
-
-
-        // Drive forward into the base.
-        encoderDrive(DRIVE_SPEED,  -10,  -10, 2.0);  // S1: Forward 24 Inches with 5 Sec timeout
-
-
-        encoderDrive(DRIVE_SPEED,  -3,  4, 2.0);  // S1: Forward 24 Inches with 5 Sec timeout
-
-
-        encoderDrive(DRIVE_SPEED,  -15,  -15, 2.0);  // S1: Forward 24 Inches with 5 Sec timeout
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-
-    }
 
     public void drive(double speed, double distance) {
 
@@ -112,7 +57,6 @@ public class AutonomousStart extends LinearOpMode {
             speed = Range.clip(Math.abs(speed), 0.0, 1.0);
             robot.leftDrive.setPower(speed);
             robot.rightDrive.setPower(speed);
-
 
             while (opModeIsActive() &&
                     robot.leftDrive.isBusy() && robot.rightDrive.isBusy()) {}
@@ -148,10 +92,10 @@ public class AutonomousStart extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Define the target for the robot to reach.
-            int leftMoveCounts = (int) (leftDistance * 1);
+            int leftMoveCounts  = (int) (leftDistance * 1);
             int rightMoveCounts = (int) (rightDistance * 1);
-            int newLeftTarget = robot.leftDrive.getCurrentPosition() + leftMoveCounts;
-            int newRightTarget = robot.rightDrive.getCurrentPosition() + rightMoveCounts;
+            int newLeftTarget   = robot.leftDrive.getCurrentPosition()  + leftMoveCounts;
+            int newRightTarget  = robot.rightDrive.getCurrentPosition() + rightMoveCounts;
 
             // Set the target positions for the robot, and set the robots to target mode.
             robot.leftDrive.setTargetPosition(newLeftTarget);
@@ -213,7 +157,7 @@ public class AutonomousStart extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newLeftTarget  = robot.leftDrive.getCurrentPosition()  + (int) (leftInches * COUNTS_PER_INCH);
             newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
             robot.leftDrive.setTargetPosition(newLeftTarget);
             robot.rightDrive.setTargetPosition(newRightTarget);
@@ -255,5 +199,50 @@ public class AutonomousStart extends LinearOpMode {
         }
 
     }
-}
+    public void initialize() {
 
+        robot.init(hardwareMap);
+
+        robot.leftClaw.setPosition(1);
+
+        telemetry.addData(">", "Robot Ready.");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+    }
+
+    public void dismount() {
+
+        // Lower the lift that holds the robot to the lander.
+
+        moveLift(1, 3.4);
+
+        // Drive away *slightly* from the lander.
+
+        turn(-.5, .5);
+
+        drive(.5, -180);
+
+        turn(-.5, 1.5);
+
+        // Drive out of the lander zone, but not so far as to disturb the minerals on the field.
+
+        drive(1, 360);
+
+    }
+
+    public void depositMarker() {
+
+        runtime.reset();
+
+        //Deposit the team marker into the team base.
+
+        robot.leftClaw.setPosition(.5);
+
+        while(runtime.seconds() <= 1) {}
+
+        robot.leftClaw.setPosition(1);
+
+    }
+}
